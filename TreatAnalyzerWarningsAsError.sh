3ede48bf00d444a352a36bfdce4de5a580432435
Michael Vosseller <michael.vosseller@gmail.com>
2013-08-29 18:16:07 -0400

    Fixed for Xcode 5

--- TreatAnalyzerWarningsAsError.sh
+++ TreatAnalyzerWarningsAsError.sh
@@ -1,4 +1,22 @@
+##
+## This script was originaly developed by Otto
+## http://stackoverflow.com/users/148076/otto
+##
+## and posted on stackoverflow.com here:
+## http://stackoverflow.com/questions/5033417/how-to-treat-warnings-from-clang-static-code-analysis-as-errors-in-xcode-3
+##
+## IMPORTANT: You eeded to enable the "Write Link Map File" build setting for this script to work
+##
+##
+
 error_count=0
+
+## The static analyzer appears to run with the first architecture listed in ARCHS (which isn't always equal to $CURRENT_ARCH)
+## This occurs when for example we build a fat universal binary ("armv7 armv7s")
+## In this example the static analyzer runs and outputs to armv7 directory but when this script is run $CURRENT_ARCH is set to armv7s
+## So when looking for the static analysis results we now use the first architecture listed in $ARCHS
+archs_array=($ARCHS)
+analyzer_arch=${archs_array[0]}
 
 ##
 
@@ -17,7 +35,7 @@ function verify_clang_analysis_at_path()
 function verify_clang_analysis_for_object_file()
 {
   local object_file=$1
-  local analysis_directory=$TARGET_TEMP_DIR/StaticAnalyzer/$CURRENT_VARIANT/$CURRENT_ARCH
+  local analysis_directory=$TARGET_TEMP_DIR/StaticAnalyzer/$PROJECT_NAME/$PRODUCT_NAME/$CURRENT_VARIANT/$analyzer_arch
   local analysis_path=$analysis_directory/${object_file%.*}.plist
 
   # if this object file corresponds to a source file that clang analyzed...
